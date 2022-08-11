0b1bc0d3e1f6fa7597235c49955d102ada546ca4
用49269794@qq.com <492697494@qq.com>
2022-08-11 14:25:00 +0800

    feat：添加vite环境变量提示

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,20 +1,19 @@
 /// <reference types="vite/client" />
 
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
-
 /**
  * 添加.env.* 环境变量提示
  */
 interface ImportMetaEnv extends Readonly<Record<string, string>> {
-  readonly VITE_APP_API_BASE_URL: string,
-  readonly VUE_APP_SERVER_HOST: string,
+  readonly VITE_APP_API_BASE_URL: string;
+  readonly VUE_APP_SERVER_HOST: string;
   // 更多环境变量...
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
