fa4cac7f241fa06e055badd47b7410c709f3e58f
用49269794@qq.com <492697494@qq.com>
2022-08-11 14:23:11 +0800

    feat: 添加prettier配置

---
+++ .prettierrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  tabWidth: 2,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: true,
+  printWidth: 100,
+  singleQuote: true,
+  semi: true,
+  overrides: [
+    {
+      files: '*.json',
+      options: {
+        printWidth: 200,
+      },
+    },
+  ],
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  vueIndentScriptAndStyle: false,
+  trailingComma: 'all',
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+};
