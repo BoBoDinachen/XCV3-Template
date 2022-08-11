b48743f8d626bdd39a06b82a060215e6d49c29a7
用49269794@qq.com <492697494@qq.com>
2022-08-11 14:24:29 +0800

    feat：添加taiwind-css配置

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+  corePlugins:{
+    // 禁用Taiwindcss提供的基础样式，方便引入组件库时的样式覆盖
+    preflight: false 
+  }
+};
